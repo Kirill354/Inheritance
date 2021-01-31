8c68ae90caa819ce28cfbaccea2d3d21c78657fb
Angry_Programist <Kirill_071@bk.ru>
2021-01-31 14:33:30 +0300

    поправил Manager

--- main.cpp
+++ main.cpp
@@ -5,26 +5,24 @@ using namespace std;
 
 class Employee {
 private:
-    double wage;
+    double m_wage;
 public:
-    virtual void SetWage(int a) {
-        wage = a;
-    }
+    Employee(int wage = 0): m_wage(wage) {}
 
-    virtual void PrintWage(){
-        cout << " Employee's wage " << endl;
+    virtual void GetWage(){
+        cout << " Employee's wage =  " << m_wage << endl;
     }
 };
 
 
 class Manager: public Employee{
 private:
-    const double wage;
+    const int m_wage = 13000;
 public:
-    Manager(): wage{13000} {}
+    Manager(){}
 
-    void PrintWage() override{
-        cout << "Manager's wage = " << wage << endl;
+    void GetWage() override{
+        cout << "Manager's wage = " << m_wage << endl;
     }
 };
 
@@ -32,13 +30,10 @@ class Agent: public Employee{
 private:
     double m_summury;
 public:
-    Agent(int summury = 0): m_summury(summury) {}
+    Agent(int summury = 0): Employee(), m_summury(summury) {}
 
-    void SetWage(int summury) override{
-        m_summury = summury;
-    }
-    void PrintWage() override{
-        cout << "Agent's wage = " << 5000 + 0.05*m_summury << endl;
+    void GetWage() override{
+        cout << "Agent's wage = " << 5000 + 0.05*m_summury <<  endl;
     }
 
 };
@@ -49,10 +44,7 @@ private:
 public:
     Worker(int hour = 0): m_hour(hour) {}
 
-    void SetWage(int hour) override{
-        m_hour = hour;
-    }
-    void PrintWage() override{
+    void GetWage() override{
         cout << "Worker's wage = " << m_hour*100 << endl;
     }
 
@@ -63,38 +55,40 @@ public:
 
 int main(){
     
+    /*
     Employee *mas = new Employee[9];
     for (int i = 0; i < 9; i++){
         if (i >= 0 && i <=2){
-            Manager *a = new Manager[i];
+            mas[i] = Manager;
         }
         else if (i >= 3 && i <= 5){
             cout << " input Agent's summury: ";
             int x; cin >> x;
             Agent *a = new Agent[i];
-            a->SetWage(x);
+            a->GetWage();
         }
         else{
             cout << " input Worker's hours: ";
             int x; cin >> x;
             Worker *b = new Worker[i];
-            b->SetWage(x);
+            b->GetWage();
         }
     }
 
     for (int i = 0; i < 9; i++){
-        mas[i].PrintWage();
+        mas[i].GetWage();
         cout << '\n';
     }
-    
-    /*
-    Manager c;
-    c.PrintWage();
-    Agent a(80000);
-    a.PrintWage();
-    Worker b(80);
-    b.PrintWage();
     */
-    delete [] mas;
+
+    
+    Manager c;
+    c.GetWage();
+    Agent a(80000);
+    a.GetWage();
+    Worker b(80);
+    b.GetWage();
+    
+    //delete [] mas;
     return 0;
 }
